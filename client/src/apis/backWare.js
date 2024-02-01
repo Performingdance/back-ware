dd56b5a0b8bfb8cd237559a4ea1158b68e986b36
Performingdance <115105039+Performingdance@users.noreply.github.com>
2024-02-01 16:48:50 +0100

    server change

--- client/src/apis/backWare.js
+++ client/src/apis/backWare.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 //const BASE_URL = 'http://localhost:3000/api/';
-const BASE_URL = 'https://api.3gger.synology.me/api/';
+const BASE_URL = 'https://back-ware-api.onrender.com';
 
 export default axios.create({
     baseURL: BASE_URL,
