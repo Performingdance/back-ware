f0b7d1e4ca6b2b29d13c176c64dc29c426e75ff7
Performingdance <115105039+Performingdance@users.noreply.github.com>
2024-02-02 22:59:40 +0100

    0.1.2

--- client/src/hooks/handleRecipeFormExtRequest.js
+++ client/src/hooks/handleRecipeFormExtRequest.js
@@ -25,16 +25,26 @@ export default function handleRecipeFormRequest(recipeID, editForm, newForm) {
                 "recipeID": recipeID
             }
         }).then((response)=>{
+            console.log(response.data)
             if(response.data.length > 0){
             response.data.forEach((obj) => {
-                obj.formweight = obj.formweight.replace(".", ","),
-                obj.worktime = obj.worktime.replace(".", ","),
-                obj.workamount = obj.workamount.replace(".", ","),
+                if(obj.formweight > 0){
+                    obj.formweight = obj.formweight.replace(".", ",")
+                }
+               if(obj.worktime > 0){
+                    obj.worktime = obj.worktime.replace(".", ",")
+               }
+               if(obj.workamount > 0){
+                    obj.workamount = obj.workamount.replace(".", ",")
+               }
+               if(obj.vkp_netto > 0){
                 obj.vkp_netto = obj.vkp_netto.replace(".", ",")
+               }
                 return obj
             })
         }
             setRes(response.data)
+            console.log(response.data)
         }).catch((err) => {
             setError(err)
             //console.log(err);
