257ffc8df7161ea8e9c3287e7a1d319b64fdd2d9
Performingdance <115105039+Performingdance@users.noreply.github.com>
2024-02-04 21:02:44 +0100

    0.1.51

--- server/routes/base.js
+++ server/routes/base.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.all((req, res, next) => {
+router.all("/", (req, res) => {
     return(
         <div>
             <h1>Back-Ware API</h1>
