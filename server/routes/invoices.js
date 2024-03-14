7bcc005e849df89bff5aaf494653f1b529389620
Performingdance <115105039+Performingdance@users.noreply.github.com>
2024-03-14 15:19:24 +0100

    0.1.561

--- server/routes/invoices.js
+++ server/routes/invoices.js
@@ -86,6 +86,18 @@ router.get("/unpaid", isLoggedIn, (req, res) => {
          }
     });
 });
+router.get("/invoiceno", isLoggedIn, (req, res) => {
+    db.query(`SELECT ID, clientID, CONCAT("#",invoice_number, DATE_FORMAT(invoice_date , "%d.%m.%y")) AS name, is_paid
+     FROM invoices WHERE is_paid IS null `, (err, result) =>{
+         if(err){
+            console.log(err)
+         } else {
+            res.send(result)
+
+            
+         }
+    });
+});
 // add item from catalog to invoice
 router.post("/new/item", isLoggedIn, (req, res, next) => {
 
