552653e2d11f06249ff273d1bb32b3176145b54a
Performingdance <115105039+Performingdance@users.noreply.github.com>
2024-03-24 11:20:21 +0100

    0.1.582177

--- client/src/apis/backWare.js
+++ client/src/apis/backWare.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
-
+import config from '../config.json'
 
 //const BASE_URL = 'http://localhost:3000/api/';
-const BASE_URL =" https://back-ware-api.onrender.com/api/";
+const BASE_URL_API = config.BASE_URL_API
 
 export default axios.create({
-    baseURL: BASE_URL,
+    baseURL: BASE_URL_API+"/api",
     timeout: 3000,
     headers: {
         'Content-Type': 'application/json',
