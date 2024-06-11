c76a06783a4fec40ae71a54516d0770457748471
Performingdance <115105039+Performingdance@users.noreply.github.com>
2024-06-11 20:24:34 +0200

    0.1.582222

--- server/routes/invoices.js
+++ server/routes/invoices.js
@@ -423,7 +423,7 @@ router.put("/new", isLoggedIn, (req, res, next) => {
         };
         
     })
-    res.send(invoice_number, invoiceID)
+    res.send({invoice_number: invoice_number, invoiceID : invoiceID})
 });
 
 
